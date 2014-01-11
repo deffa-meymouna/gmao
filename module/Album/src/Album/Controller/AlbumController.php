0c4bbd7d7ee75d65a29c2d06589bc322e55cc17c
Alexandre Tranchant <Alexandre.Tranchant@gmail.com>
2014-01-11 21:14:22 +0100

    Database connection and Album Controller Index Action

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -10,12 +10,27 @@
 namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class AlbumController extends AbstractActionController
 {
+
+	protected $albumTable;
+	
+	public function getAlbumTable()
+	{
+		if (!$this->albumTable) {
+			$sm = $this->getServiceLocator();
+			$this->albumTable = $sm->get('Album\Model\AlbumTable');
+		}
+		return $this->albumTable;
+	}
+	
     public function indexAction()
     {
-        return array();
+        return new ViewModel(array(
+             'albums' => $this->getAlbumTable()->fetchAll(),
+         ));
     }
 
     public function addAction()
