94884e6129925f2ec322b9d69e522b5095138270
Alexandre T <alexandre.Tranchant@gmail.com>
2015-07-11 21:33:54 +0200

    Update init_autoloader.php
    Reprise de la version de Zend-Skeleton

--- init_autoloader.php
+++ init_autoloader.php
@@ -3,10 +3,9 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 /**
  * This autoloading setup is really more complicated than it needs to be for most
  * applications. The added complexity is simply to reduce the time it takes for
@@ -15,25 +14,23 @@
  * the use of composer completely optional. This setup should work fine for
  * most users, however, feel free to configure autoloading however you'd like.
  */
-
 // Composer autoloading
 if (file_exists('vendor/autoload.php')) {
     $loader = include 'vendor/autoload.php';
 }
-
+if (class_exists('Zend\Loader\AutoloaderFactory')) {
+    return;
+}
 $zf2Path = false;
-
-if (is_dir('vendor/ZF2/library')) {
-    $zf2Path = 'vendor/ZF2/library';
-} elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
+if (getenv('ZF2_PATH')) {            // Support for ZF2_PATH environment variable
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
     $zf2Path = get_cfg_var('zf2_path');
 }
-
 if ($zf2Path) {
     if (isset($loader)) {
         $loader->add('Zend', $zf2Path);
+        $loader->add('ZendXml', $zf2Path);
     } else {
         include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
         Zend\Loader\AutoloaderFactory::factory(array(
@@ -43,7 +40,6 @@ if ($zf2Path) {
         ));
     }
 }
-
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
 }
