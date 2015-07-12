d562d12b84d5195762ce10ad43679da00ce7a514
Alexandre Tranchant <Alexandre.Tranchant@gmail.com>
2015-07-12 20:21:43 +0200

    Comments and todo-list updated

--- module/Administration/src/Administration/Controller/IndexController.php
+++ module/Administration/src/Administration/Controller/IndexController.php
@@ -8,11 +8,15 @@ use Zend\View\Model\ViewModel;
 class IndexController extends AbstractActionController
 {
 
+    /**
+     * Action Index qui servira de dashboard
+     * 
+     * @todo Realize Dashboard
+     * @see \Zend\Mvc\Controller\AbstractActionController::indexAction()
+     */
     public function indexAction()
     {
         return new ViewModel();
     }
-
-
 }
 
